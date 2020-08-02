8a36fde8c53ffce88a16cb7f0ffb579c6dfdf855
AshtonBurnside <69077259+AshtonBurnside@users.noreply.github.com>
2020-08-02 21:30:35 +0800

    Jump
    Added player jump to the Player_Move script.

--- Brackeys Game Jam Game/Assets/Scripts/Player_Move.cs
+++ Brackeys Game Jam Game/Assets/Scripts/Player_Move.cs
@@ -5,16 +5,17 @@ public class Player_Move : MonoBehaviour
     public Rigidbody rb;
     public float speed;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Moves the players position depending on the inputs of WASD.
         rb.velocity = Input.GetAxis("Horizontal") * speed * transform.right + new Vector3(0, rb.velocity.y, 0) + Input.GetAxis("Vertical") * speed * transform.forward;
+
+        //Only activates upon pressing space if the player is > 1.3 from the ground
+        if (Input.GetKey(KeyCode.Space) && Physics.Raycast(transform.position, transform.up * -1, 1.3f))
+        {
+            //adds an upward force to the player (Jump).
+            rb.AddForce(new Vector3(0, 1, 0), ForceMode.Impulse);
+        }
     }
 }
