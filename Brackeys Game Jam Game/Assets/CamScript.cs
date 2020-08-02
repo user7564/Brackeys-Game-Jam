2976254b309cbd4804bca39d653b38435a179ea8
user7564 <50976772+user7564@users.noreply.github.com>
2020-08-02 15:54:47 +0800

    Fixed move and cam cs

--- Brackeys Game Jam Game/Assets/CamScript.cs
+++ Brackeys Game Jam Game/Assets/CamScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CamScript : MonoBehaviour
 {
+    [SerializeField]
+    Camera MainCam;
+    float MouseX, MouseY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MouseX = Input.GetAxis("Mouse X");
+        MouseY = Input.GetAxis("Mouse Y") * -1;
+        transform.Rotate(new Vector3(0, MouseX, 0));
+        MainCam.transform.Rotate(new Vector3(MouseY, 0, 0));
+        if (Application.isFocused) Cursor.lockState = CursorLockMode.Locked; else Cursor.lockState = CursorLockMode.None;
+
     }
 }
