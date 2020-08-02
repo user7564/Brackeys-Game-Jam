f2856089b1ed53b7a4ff6a1773d6ac2c74746bf6
user7564 <50976772+user7564@users.noreply.github.com>
2020-08-02 15:20:40 +0800

    Created Camera Script

--- Brackeys Game Jam Game/Assets/Move.cs
+++ Brackeys Game Jam Game/Assets/Move.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField]
+    Camera MainCam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainCam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        MainCam.transform.Rotate();
+
+    }
+
+    void LockOrUnlockCursor(CursorLocked)
+    {
+        if (CursorLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
