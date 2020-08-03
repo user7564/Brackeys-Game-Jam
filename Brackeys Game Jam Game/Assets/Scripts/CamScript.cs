c92d8624acf5e43db00011265242c7399db57a95
AshtonBurnside <69077259+AshtonBurnside@users.noreply.github.com>
2020-08-03 22:11:35 +0800

    Replaced UI script
    Created new seperate UI script and removed the old one.

--- Brackeys Game Jam Game/Assets/Scripts/CamScript.cs
+++ Brackeys Game Jam Game/Assets/Scripts/CamScript.cs
@@ -7,37 +7,17 @@ public class CamScript : MonoBehaviour
     [SerializeField]
     Camera MainCam;
     float MouseX, MouseY;
-    bool EscapeMenu;
-    Player_Move movescript;
-    bool unlockdone;
-    [SerializeField]
-    Canvas pausemenu;
-    bool lockedcursor;
+
     // Start is called before the first frame update
     void Start()
     {
         MainCam = Camera.main;
-        movescript = GetComponent<Player_Move>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape)) pausemenu.gameObject.SetActive(true);
+    {    
 
-        if (pausemenu.gameObject.activeSelf)
-        {
-            movescript.Movelock(true);
-            Cursor.lockState = CursorLockMode.None;
-            return;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            movescript.Movelock(false);
-            pausemenu.gameObject.SetActive(false);
-        }
-        
         MouseX = Input.GetAxis("Mouse X");
         MouseY = Input.GetAxis("Mouse Y") * -1;
         transform.Rotate(new Vector3(0, MouseX, 0));
---
+++ Brackeys Game Jam Game/Assets/Scripts/UI.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    private bool Menu_Active = false;
+    public GameObject Ui;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ui.gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Menu_Active == false)
+            {
+                pause();
+            }
+
+            else
+            {
+                resume();
+            }
+        }
+    }
+    public void pause()
+    {
+        Menu_Active = true;
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Ui.gameObject.SetActive(true);
+    }
+
+    public void resume()
+    {
+        Menu_Active = false;
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Ui.gameObject.SetActive(false);
+    }
+
+    public void exit()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
